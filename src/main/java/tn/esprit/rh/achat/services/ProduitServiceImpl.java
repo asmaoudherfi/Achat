ae87089197ef6a459fbff6470e4c51e597f32725
alaaeddinezarrouk <alaaeddine.zarrouk@esprit.tn>
2023-10-14 13:19:29 +0100

    correcting bug

--- src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
@@ -61,7 +61,9 @@ public class ProduitServiceImpl implements IProduitService {
 	public void assignProduitToStock(Long idProduit, Long idStock) {
 		Produit produit = produitRepository.findById(idProduit).orElse(null);
 		Stock stock = stockRepository.findById(idStock).orElse(null);
-		produit.setStock(stock);
+		if (produit != null && stock != null) {
+			produit.setStock(stock);
+		}
 		produitRepository.save(produit);
 
 	}
